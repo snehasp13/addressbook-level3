2200dadcaeba0309a4aca2d75eb585f4f90180f9
snehasp13 <snehasp01@gmail.com>
2016-10-08 07:59:37 +0800

    Sort command_edited

--- src/seedu/addressbook/commands/Sort.java
+++ src/seedu/addressbook/commands/Sort.java
@@ -1,25 +1,30 @@
 package seedu.addressbook.commands;
 
+import java.text.Collator;
 import java.util.Collections;
 import java.util.List;
 
-import seedu.addressbook.common.Messages;
+import seedu.addressbook.data.person.Name;
 import seedu.addressbook.data.person.Person;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
 public class Sort extends Command {
 	public static final String COMMAND_WORD = "sort entries";
-	public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" 
+	public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n"
             + "Displays all the entries in alphabetical order.\n\t";
-	public static final String MESSAGE_SUCCESS = "Sorted List: %1$s";
-	
-	
-	 
-	 @Override
+	public static final String MESSAGE_SUCCESS = "Sorted List in alphabetical order";
+
+	public Sort(ReadOnlyPerson allPersons){
+		super();
+	}
+
+	@Override
 	  public CommandResult execute() {
-	        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
-	        Collections.sort((List<ReadOnlyPerson>) allPersons);
-	        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+			List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+	       // List<Name> allPersonsNames = addressBook.getAllPersons().immutableListView();
+			List<Name> allPersonsNames = (List<Name>) ((Person) allPersons).getName();
+	        Collections.sort(allPersonsNames, Collator.getInstance());
+	        //return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+	        return new CommandResult(MESSAGE_SUCCESS, allPersons);
 	    }
-		
 }
